f6acc7ff6f0a83465b3e666c33a434bcc2a2ce98
Praveen Vaddadi <h.praveen.vaddadi@accenture.com>
2018-08-05 19:31:02 +0530

    modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AppRoutingModule } from './shared/app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CustomMaterialDesignModule } from './shared/custom-material-design.module';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
+
+import {FormsModule} from '@angular/forms';
+import { CommercialComponent } from './commercial/commercial.component';
+	
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    CommercialComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialDesignModule,
+    FormsModule,
+    CustomerComponent
+
+  ],
+  exports: [
+    CustomMaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
