2c74b5ca8e9bde133fbee2f663bcf4ed9f077227
Praveen Vaddadi <h.praveen.vaddadi@accenture.com>
2018-08-18 10:58:55 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
 import { AppRoutingModule } from './shared/app-routing.module';
 
 import {FormsModule} from '@angular/forms';
-import { CommercialComponent } from './commercial/commercial.component';
 import { SharedModule } from './shared/shared.module';
 import { CustomerModule } from './customer/customer.module';
+import { CommercialComponent } from './commercial/commercial.component';
 
 	
 @NgModule({
   declarations: [
     AppComponent,
-    CommercialComponent,
-    CustomerComponent
+    CommercialComponent
+
    
   ],
   
